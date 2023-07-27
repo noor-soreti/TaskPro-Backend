750670edb44071c26370a4ddf799a19320ecb8e7
Noor Soreti <noor.said0698@gmail.com>
2023-07-27 13:35:20 -0400

    .gitignore

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const userRouter = require('./route/UserRoute.js')
 const mongoose = require('mongoose');
-const server = require('./schema/index.js')
 require('dotenv').config();
 
 
@@ -14,11 +12,3 @@ mongoose.connect(process.env.MONGODB_CONNECT, {
 }).catch(err => {
     console.log('Error Mongodb connection')
 });
-
-// app.use(userRouter);
-
-// app.listen(3000, () => {
-//     console.log('listening on *:3000');
-// });
-
-
