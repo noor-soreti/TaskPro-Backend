e75307583dacda9d9637b4a93090a9dd6a26323a
Noor Soreti <noor.said0698@gmail.com>
2023-09-19 14:21:26 -0400

    again

--- schema/resolver/Resolvers.js
+++ schema/resolver/Resolvers.js
@@ -66,7 +66,7 @@ export const resolvers = {
 
             } catch (e) {
                 console.error('Registration error:', e.message);
-                throw new Error('Registration error:', e.message);
+                throw new Error('Unable to register.');
             }
         },
         addTask: async (_, { title, dueDate, setDate, priority }, context) => {
